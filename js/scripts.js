97a8c9048ef99c0353fb6d6a60e71d621b25ab2d
rhorecny <129782304+rhorecny@users.noreply.github.com>
2023-10-18 14:15:05 -0400

    scripts.js completely rewritten
    Removed original text in scripts.js and added pokemonList arrays for pokedex project

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-//header alert
-alert('hello world!');
-//variables
-let favoriteFood = 'Pizza';
-//writing to HTML
-document.write(favoriteFood);
+//pokedex arrays
+let pokemonList= [
+    {name: 'Arcanine', height: 6, type: 'Fire'},
+    {name: 'Slowbro', height: 5, type: ['Water', 'Psychic']},
+    {name: 'Mew',height: 1, type: 'Psycic'}
+];
