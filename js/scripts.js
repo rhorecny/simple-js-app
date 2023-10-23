180eac26600a08e4dde150348ce33f4f9d0777f1
rhorecny <129782304+rhorecny@users.noreply.github.com>
2023-10-23 15:49:16 -0400

    Changes to index.html, styles.css, scripts.js
    Added link to styles.css in index.html, stylized body and <p> in styles.css to look more pleasing, and created <p> elements in scripts.js that displays pokemon and height, along with conditionals based on height.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ let pokemonList= [
     {name: 'Slowbro', height: 5, type: ['Water', 'Psychic']},
     {name: 'Mew',height: 1, type: 'Psycic'}
 ];
+
+//Loop lists pokemon name and height on index.html, and highlights if the pokemon is a big pokemon based on size. <p> tags added for list
+for (let i = 0; i < pokemonList.length; i++){
+    document.write(`<p>${pokemonList[i].name} (Height: ${pokemonList[i].height})`);
+ if (pokemonList[i].height > 5) {
+    document.write(' - Wow, that\'s a big Pokemon!</p>');
+} else {
+    document.write('</p>');
+}
+} 
