32e79f14c9cfc597ac914006475cfc112ba9c577
rhorecny <129782304+rhorecny@users.noreply.github.com>
2023-11-02 16:41:49 -0400

    Updates to script.js
    wrapped pokemonList into an IIFE , created returns 'getAll' and 'add' to return all values in array, and allow to add to array. Pushed new object into array, logged pokemonRepository.getAll to console, added document.write and an arguemnt of what text to display based on the height value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
-//pokedex arrays
-let pokemonList= [
-    {name: 'Arcanine', height: 6, type: 'Fire'},
-    {name: 'Slowbro', height: 5, type: ['Water', 'Psychic']},
-    {name: 'Mew',height: 1, type: 'Psycic'}
+//pokedex arrays wrapped in IIFE allows add new function and a view all funciton
+let pokemonRepository = (function () {
+    let pokemonList= [
+    {name: 'Arcanine', height: 6.03, type: ['Fire']},
+    {name: 'Slowbro', height: 5.03, type: ['Water', 'Psychic']},
+    {name: 'Mew',height: 1.04, type: ['Psycic']}
 ];
 
-//Loop lists pokemon name and height on index.html, and highlights if the pokemon is a big pokemon based on size. <p> tags added for list
-function pokemonLoopFunction(pokemon) {
-    document.write('<p>' + pokemon.name + ' has a height of ' + pokemon.height )
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+function getAll() {
+    return pokemonList;
+}
+
+return {
+    add: add,
+    getAll: getAll
 };
-pokemonList.forEach(pokemonLoopFunction);
+})()
+
+//added value to pokemonRepository using the following format
+pokemonRepository.add({name: 'Rattata', height: 1, type:['Normal']});
+//execute .getAll command
+console.log(pokemonRepository.getAll());
+
+//logic to decipher if pokemon size is large or not
+pokemonRepository.getAll().forEach(function(pokemon){
+    if (pokemon.height > 5) {
+        document.write(pokemon.name + " has a height of " + pokemon.height + '- wow, that\'s a big pokemon!' + '<br>')
+    
+} else {
+    document.write(pokemon.name + ' has a height of ' + pokemon.height + '<br>');
+    }
+});
