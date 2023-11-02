03fd40c3a12667c38cfc2993254d2ed8cbf3d8e6
rhorecny <129782304+rhorecny@users.noreply.github.com>
2023-11-02 15:47:58 -0400

    Revised script.js
    Changed for loop to foreach for optimization of code.

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,7 @@ let pokemonList= [
 ];
 
 //Loop lists pokemon name and height on index.html, and highlights if the pokemon is a big pokemon based on size. <p> tags added for list
-for (let i = 0; i < pokemonList.length; i++){
-    document.write(`<p>${pokemonList[i].name} (Height: ${pokemonList[i].height})`);
- if (pokemonList[i].height > 5) {
-    document.write(' - Wow, that\'s a big Pokemon!</p>');
-} else {
-    document.write('</p>');
-}
-} 
+function pokemonLoopFunction(pokemon) {
+    document.write('<p>' + pokemon.name + ' has a height of ' + pokemon.height )
+};
+pokemonList.forEach(pokemonLoopFunction);
